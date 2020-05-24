3a79355f84a73e827a3be978dd8ffc9bd1f45fdc
ckcharchit <ckcharchit@gmail.com>
2020-05-24 18:02:19 +0530

    Files Added

---
+++ SearchImage/Utilities/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SearchImage
+//
+//  Created by CHARCHIT KUMAR on 24/05/20.
+//  Copyright © 2020 CHARCHIT KUMAR. All rights reserved.
+//
+
+import Foundation
+
+let kPageSize = 50
+let kUnknownError = "An Unknown error has occured"
+let kNoInternetConnection = "Please check your Internet connection and try again."
+
+let kFlickrAPIKey = "2c621073cb6aa5c12ef5aa14aa4cdcb0"
+let kFlickrAPISecret = "db808e1145d70dce"
+
+let kBaseURL = "https://pixabay.com/api/?key=16576202-62c78c26f5e194d7f7842a23b"
+
+let kSearchURL = "\(kBaseURL)&q=%@&&image_type=photo"
+
+let kImageURL = "https://pixabay.com/get/%@_%@_%@.jpg.jpg"
---
+++ SearchImage/Utilities/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  SearchImage
+//
+//  Created by CHARCHIT KUMAR on 24/05/20.
+//  Copyright © 2020 CHARCHIT KUMAR. All rights reserved.
+//
+
+import Foundation
+
+enum CallBackType {
+    case reload
+    case showLoader
+    case hideLoader
+    case showError(error: String)
+}
